db078f172bb66c989f5d906c32de46f2ce19c7c8
Turan <turanaymis@gmail.com>
2024-06-09 21:51:49 +0300

    2D List and Nested Loops

---
+++ 20_ForLoops.py
@@ -0,0 +1,26 @@
+for letter in "Turan Aymış":
+    print(letter)
+
+print("------")
+friends = ["Aydın", "Görkem", "Onur"]
+for name in friends:
+    print(name)
+
+print("------")
+for index in range(10):
+    print(index)
+
+print("------")
+for index in range(3, 10):
+    print(index)
+
+print("------")
+for index in range(len(friends)):
+    print(friends[index])
+
+print("------")
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("Not first")
